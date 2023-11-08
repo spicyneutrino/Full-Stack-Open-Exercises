9a6a5552a5044c5f40ad2c1f84ed13ee204f0b32
spicyneutrino <107169289+spicyneutrino@users.noreply.github.com>
2023-11-07 19:00:42 -0600

    Complete Course-Info

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,11 +1,67 @@
+const Header = ({ course }) => {
+
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <>
+      <div>{part.name} {part.exercises}</div>
+    </>
+  )
+}
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  return(
+    <div>
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </div>
+  )
+}
+
+
+
+
 
 const App = () => {
   {console.clear()}
-  return (
-    <>
-      <h1>Greetings</h1>
-    </>
-  )
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+return (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
 }
 
 export default App
